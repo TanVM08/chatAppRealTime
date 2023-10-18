cdbc7445fdb6c6da6061c4d8568a20a3699f35bb
TanVM <vuminhtan0103@gmail.com>
2023-10-19 02:12:28 +0700

    commit chat service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
+import { ProfileUser } from 'src/app/models/user';
+import { ChatsService } from 'src/app/services/chats.service';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -8,15 +9,43 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  userCurent: any
   lstUser: any = [];
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private chatService: ChatsService) { }
 
   ngOnInit(): void {
+    this.getUserCurent();
     this.getAllUsers();
+  }
+
+  getUserCurent() {
+    this.usersService.currentUserProfile$.subscribe(res => {
+      this.userCurent = res;
+    })
   }
   getAllUsers() {
     this.usersService.getAllUser().subscribe((res: any) => {
       this.lstUser = res;
     });
+  }
+
+  createChat(item: ProfileUser) {
+    debugger
+    let chat: object = {
+      userIds: [this.userCurent?.uid, item.uid],
+      users: [
+        {
+          displayName: this.userCurent?.displayName ?? '',
+          photoUrl: this.userCurent?.photoUrl ?? ''
+        },
+        {
+          displayName: item?.displayName ?? '',
+          photoUrl: item?.photoURL ?? ''
+        }
+      ]
+    }
+    this.chatService.createChat(chat);
   }
 }
--- src/app/services/chats.service.ts
+++ src/app/services/chats.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Firestore, addDoc, collection } from 'firebase/firestore';
+import { AuthService } from './auth.service';
+import { Observable, from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatsService {
 
-  constructor() { }
+  constructor(
+    private firestore: Firestore,
+    private authService: AuthService
+  ) { }
+
+  createChat(chat: any):Observable<any> {
+    const ref = collection(this.firestore, 'chats');
+    return from(addDoc(ref, chat));
+  }
 }
